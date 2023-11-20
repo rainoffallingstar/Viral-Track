384f7ffe1ab33747d514f8e79c49c657f9bce547
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2023-11-20 15:36:36 +0800

    update parameters

--- runworkflow.sh
+++ runworkflow.sh
@@ -15,25 +15,42 @@ cat /public3/home/scg5695/project/15_scCRC/multi/Fastq/mRNA/*_R1_001.fastq.gz > 
 cat /public3/home/scg5695/project/15_scCRC/multi/Fastq/mRNA/*_R2_001.fastq.gz > crcsinglecell_R2.fastq.gz
 
 # umi bc
-# 哪种测序方式？一个样本大概多少细胞？
+# 哪种测序方式？一个样本大概多少细胞？--》 10X，可以自动估计细胞
 
 # step 2
 # about bc-pattern
-zcat hgmm_100_R1.fastq.gz | head -n2
+zcat crcsinglecell_R1.fastq.gz | head -n2
 
-umi_tools whitelist --crcsinglecell_R1.fastq.gz \
-                     --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN \ 
-                     --log2stderr > whitelist.txt
+umi_tools whitelist --stdin crcsinglecell_R1.fastq.gz --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN --log2stderr > whitelist.txt
                      
 # step 3
 
 umi_tools extract --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN \
                   --stdin crcsinglecell_R1.fastq.gz \
-                  --stdout crcsinglecell_R1_extracted.fastq.gz \
+                  --stdout crcsinglecell_R1_extracted.fastq \
                   --read2-in crcsinglecell_R2.fastq.gz \
-                  --read2-out=crcsinglecell_R2_extracted.fastq.gz \
+                  --read2-out=crcsinglecell_R2_extracted.fastq \
                   --whitelist=whitelist.txt 
                      
+                     
+# viral track processing
+
+# in virus mod
+
+Rscript Viral_Track_scanning.R virus_Parameter.txt Files_to_process.txt
+Rscript Viral_Track_transcript_assembly.R virus_Parameter.txt Files_to_process.txt
+Rscript Viral_Track_cell_demultiplexing.R virus_Parameter.txt Files_to_process.txt
+
+# in germ mod 
+
+Rscript Viral_Track_scanning.R germ_Parameter.txt Files_to_process.txt
+Rscript Viral_Track_transcript_assembly.R germ_Parameter.txt Files_to_process.txt
+Rscript Viral_Track_cell_demultiplexing.R germ_Parameter.txt Files_to_process.txt
+
+
+
+
+
                      
                      
                      
