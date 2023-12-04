50bdd127f4acc9a4ef7faa535eb5afc562644eb0
fallingstarbot@chaosuan <fallingstarbot@chaosuan.net>
2023-12-04 20:25:32 +0800

    up2date

--- umi2gz.sh
+++ umi2gz.sh
@@ -12,13 +12,12 @@ while IFS= read -r sample_list <&3; do
         # 运行 umi_tools 命令
         umi_tools extract --bc-pattern=CCCCCCCCCCCCCCCCNNNNNNNNNN \
             --stdin "${input_dir}/${sample_list}_R1_001.fastq.gz" \
-            --stdout "${input_dir}/${sample_list}_R1_extracted.fastq" \
+            --stdout "${output_dir}/${sample_list}_R1_extracted.fastq" \
             --read2-in "${input_dir}/${sample_list}_R2_001.fastq.gz" \
             --read2-out="${output_dir}/${sample_list}_R2_extracted.fastq" \
             --log="${output_dir}/${sample_list}_extract_processed.log" \
             --whitelist="$barcode_file" 
         echo "cleaning"
-        rm -rf "${input_dir}/${sample_list}_R1_extracted.fastq"
         echo "正在压缩样本"
         pigz -p 16 "${output_dir}/${sample_list}_R2_extracted.fastq"
         
